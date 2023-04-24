d4b69890aa8261424c49cee12d1f849493b3cfb7
pavloyurchyshyn <payurchyshyn@gmail.com>
2023-04-24 21:36:28 +0300

    Simple top-flat oriented hexes math.

---
+++ hex_map.py
@@ -0,0 +1,145 @@
+import pygame
+from typing import List, Dict, Tuple
+
+from math import Hexagon, HexMath
+
+pygame.init()
+
+HEX_RADIUS = 50
+HORIZONTAL_GRID_SIZE = 20
+VERTICAL_GRID_SIZE = 10
+
+SCREEN_WIDTH, SCREEN_HEIGHT = HexMath.get_grid_size(h_size=HORIZONTAL_GRID_SIZE,
+                                                    v_size=VERTICAL_GRID_SIZE,
+                                                    radius=HEX_RADIUS)
+W_MID = SCREEN_WIDTH // 2
+H_MID = SCREEN_HEIGHT // 2
+size = (SCREEN_WIDTH, SCREEN_HEIGHT)
+screen = pygame.display.set_mode(size)
+
+pygame_clock = pygame.time.Clock()
+
+FONT = pygame.font.SysFont('Arial', HEX_RADIUS // 5)
+
+
+class GridHex(Hexagon):
+    def __init__(self, x_id, y_id, radius=HEX_RADIUS, border_size=1, draw_text=True):
+        super(GridHex, self).__init__(x_id=x_id, y_id=y_id, radius=radius)
+        self.border_size: int = border_size
+        self.draw_text: border_size = draw_text
+        self.text = FONT.render(f'{self.qrs}-{self.xy_id}', True, (255, 255, 255))
+
+    def draw(self):
+        pygame.draw.lines(screen, (255, 255, 255), True, self.dots, self.border_size)
+        if self.draw_text:
+            x, y = self.center
+            screen.blit(self.text, (x - self.text.get_width() // 2, y - self.text.get_height() // 2))
+
+
+class HexWorld:
+    def __init__(self):
+        self.hexes_list: List[GridHex] = []
+        self.xy_to_hex: Dict[Tuple[int, int], GridHex] = {}
+        self.qr_to_hex: Dict[Tuple[int, int], GridHex] = {}
+
+    def create_hexes(self):
+        for y, line in enumerate([[1 for _ in range(HORIZONTAL_GRID_SIZE)] for _ in range(VERTICAL_GRID_SIZE)]):
+            for x, val in enumerate(line):
+                if val:
+                    h = GridHex(x_id=x, y_id=y, radius=HEX_RADIUS)
+                    self.hexes_list.append(h)
+                    self.qr_to_hex[h.qr] = h
+                    self.xy_to_hex[h.xy_id] = h
+
+
+world = HexWorld()
+world.create_hexes()
+RANGE_RADIUS = 1
+TARGET_HEX = (0, 0)
+
+
+def draw_hex_under_mouse(h: GridHex):
+    if h.qr in world.qr_to_hex:
+        pygame.draw.lines(screen, (0, 255, 0), True, h.dots, 3)
+
+
+def draw_square(q, r):
+    neighbors = HexMath.get_neighbors_qr(q=q, r=r, range_radius=RANGE_RADIUS)
+    for qr in neighbors:
+        if qr in world.qr_to_hex:
+            pygame.draw.polygon(screen, (110, 111, 111), world.qr_to_hex[qr].dots)
+
+
+def draw_hexes_ring(q, r):
+    neighbors_at_radius = HexMath.ring_at_radius(q=q, r=r, range_radius=RANGE_RADIUS)
+    for qr in neighbors_at_radius:
+        if qr in world.qr_to_hex:
+            pygame.draw.polygon(screen, (150, 150, 150, 50), world.qr_to_hex[qr].dots)
+
+
+def draw_square_edge(q, r):
+    if dots_at_edge := HexMath.ring_at_radius_border(q=q, r=r, range_radius=RANGE_RADIUS, radius=HEX_RADIUS):
+        pygame.draw.lines(screen, (255, 100, 100), True, dots_at_edge, 5)
+
+
+def draw_from_a_to_b(a_xy, b_xy):
+    a_hex = Hexagon(*a_xy, HEX_RADIUS)
+    b_hex = Hexagon(*b_xy, HEX_RADIUS)
+
+    if line := HexMath.ray_from_a_to_b(a_hex, b_hex):
+        line = [HexMath.qr_to_xy_coords(*qr, HEX_RADIUS) for qr in line]
+        pygame.draw.lines(screen, (0, 0, 255), False, line, 6)
+
+
+def draw_target_in_range_line(a_xy, b_xy):
+    a_hex = Hexagon(*a_xy, HEX_RADIUS)
+    b_hex = Hexagon(*b_xy, HEX_RADIUS)
+    in_range = HexMath.get_cube_distance(a_hex, b_hex)
+    color = (255, 0, 0) if in_range > RANGE_RADIUS else (0, 255, 0)
+    pygame.draw.line(screen, color, a_hex.center, b_hex.center, 2)
+
+
+while True:
+    mouse_pos = pygame.mouse.get_pos()
+    screen.fill((0, 0, 0))
+    pygame.draw.line(screen, (200, 100, 100), (W_MID, 0),
+                     (W_MID, HexMath.get_grid_vertical_size(VERTICAL_GRID_SIZE, HEX_RADIUS)), 3)
+    pygame.draw.line(screen, (100, 100, 100), (W_MID, 0), (W_MID, SCREEN_HEIGHT))
+    pygame.draw.line(screen, (100, 100, 200), (0, H_MID),
+                     (HexMath.get_grid_horizontal_size(HORIZONTAL_GRID_SIZE, HEX_RADIUS), H_MID), 3)
+    pygame.draw.line(screen, (100, 100, 100), (0, H_MID), (SCREEN_WIDTH, H_MID))
+    pygame_clock.tick(60)
+
+    mouse_qr = HexMath.xy_coord_to_qr(*mouse_pos, HEX_RADIUS)
+    mouse_xy_id = HexMath.normalize_coordinates(*mouse_pos, HEX_RADIUS)
+    mouse_hex = GridHex(*mouse_xy_id, HEX_RADIUS, draw_text=False)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 4:
+                RANGE_RADIUS += 1
+
+            elif event.button == 5:
+                RANGE_RADIUS -= 1
+                if RANGE_RADIUS < 0:
+                    RANGE_RADIUS = -1
+            elif event.button == 1:
+                if mouse_xy_id in world.xy_to_hex:
+                    TARGET_HEX = mouse_xy_id
+
+    pygame.draw.polygon(screen, (100, 150, 100), world.xy_to_hex[TARGET_HEX].dots)
+
+    draw_square(*mouse_qr)
+    draw_hexes_ring(*mouse_qr)
+
+    for h in world.hexes_list:
+        h.draw()
+
+    draw_square_edge(*mouse_qr)
+    draw_hex_under_mouse(mouse_hex)
+    draw_from_a_to_b(mouse_xy_id, TARGET_HEX)
+    draw_target_in_range_line(mouse_xy_id, TARGET_HEX)
+
+    pygame.display.update()
---
+++ math.py
@@ -0,0 +1,230 @@
+# More here: https://www.redblobgames.com/grids/hexagons/
+import math
+from typing import List, Tuple, Union
+
+ANGLES: Tuple[float] = tuple([math.radians(60 * i) for i in range(6)])
+
+
+def get_dots(x: int, y: int, radius: int, dx: int = 0, dy: int = 0) -> Tuple[Tuple[int, int]]:
+    return tuple([(int(x + dx + radius * math.cos(a)), int(y + dy + radius * math.sin(a))) for a in ANGLES])
+
+
+class Cube:
+    """Class for cube coordinates, cube vector."""
+    def __init__(self, q: int, r: int, s: int = None):
+        self.q: int = q
+        self.r: int = r
+        self.s: int = -q - r if s is None else s
+
+    @property
+    def qrs(self) -> Tuple[int, int, int]:
+        return self.q, self.r, self.s
+
+    @property
+    def qr(self) -> Tuple[int, int]:
+        return self.q, self.r
+
+    def __add__(self, other: 'Cube') -> 'Cube':
+        return Cube(self.q + other.q, self.r + other.r, self.s + other.s)
+
+    def __sub__(self, other: 'Cube') -> 'Cube':
+        return Cube(self.q - other.q, self.r - other.r, self.s - other.s)
+
+    def scale(self, factor: float) -> 'Cube':
+        return Cube(int(self.q * factor), int(self.r * factor), int(self.s * factor))
+
+    def cube_neighbor(self, direction: int) -> 'Cube':
+        return self + CUBE_VECTORS[direction]
+
+
+CUBE_VECTORS: List[Cube] = [
+    Cube(+1, 0, -1), Cube(+1, -1, 0), Cube(0, -1, +1),
+    Cube(-1, 0, +1), Cube(-1, +1, 0), Cube(0, +1, -1),
+]
+
+
+class XYIdHex:
+    def __init__(self, x_id: int, y_id: int):
+        self.x_id: int = x_id
+        self.y_id: int = y_id
+
+    @property
+    def xy_id(self) -> Tuple[int, int]:
+        return self.x_id, self.y_id
+
+
+class Hexagon(XYIdHex, Cube):
+    def __init__(self, x_id, y_id, radius):
+        self.radius: int = radius
+        super(Hexagon, self).__init__(x_id=x_id, y_id=y_id)
+        Cube.__init__(self, *HexMath.xy_id_to_qr(x_id, y_id))
+
+        self.x: int = HexMath.get_x_coord(q=self.q, radius=self.radius)
+        self.y: int = HexMath.get_y_coord(q=self.q, r=self.r, radius=self.radius)
+        self.dots: Tuple[Tuple[int, int]] = None
+        self.create_dots()
+
+    @property
+    def center(self) -> Tuple[int, int]:
+        return self.x, self.y
+
+    def create_dots(self):
+        self.dots: Tuple[Tuple[int, int]] = get_dots(self.x, self.y, radius=self.radius)
+
+
+class HexMath:
+    @classmethod
+    def horizontal_spacing(cls, radius: int) -> float:
+        return cls.get_width(radius=radius) * 0.75
+
+    @classmethod
+    def vertical_spacing(cls, radius: int) -> float:
+        return cls.get_height(radius=radius)
+
+    @staticmethod
+    def get_width(radius: int) -> int:
+        return int(radius + radius)
+
+    @staticmethod
+    def get_height(radius: int) -> float:
+        return int(math.sqrt(3) * radius)
+
+    @classmethod
+    def qr_to_xy_coords(cls, q: int, r: int, radius: int) -> Tuple[int, int]:
+        return cls.get_x_coord(radius=radius, q=q), cls.get_y_coord(radius=radius, q=q, r=r)
+
+    @classmethod
+    def get_x_coord(cls, q: int, radius: int) -> int:
+        return int(q * cls.horizontal_spacing(radius=radius) + radius)
+
+    @classmethod
+    def get_y_coord(cls, q: int, r: int, radius: int) -> int:
+        return int(cls.get_height(radius=radius) * (r + q / 2) + cls.get_height(radius=radius) // 2)
+
+    @classmethod
+    def get_grid_vertical_size(cls, y: int, radius: int) -> int:
+        return int(cls.vertical_spacing(radius=radius) * y + cls.get_height(radius=radius) / 2) + 1
+
+    @classmethod
+    def get_grid_size(cls, h_size: int, v_size: int, radius: int) -> Tuple[int, int]:
+        return cls.get_grid_horizontal_size(h_size, radius), cls.get_grid_vertical_size(v_size, radius)
+
+    @classmethod
+    def get_grid_horizontal_size(cls, x: int, radius: int) -> int:
+        return int(cls.horizontal_spacing(radius) * x + cls.get_width(radius) * 0.25) + 1
+
+    @classmethod
+    def normalize_coordinates(cls, x: int, y: int, radius: int) -> Tuple[int, int]:
+        x = x // cls.horizontal_spacing(radius)
+        if x % 2:
+            y -= cls.vertical_spacing(radius) // 2
+        y = y // cls.vertical_spacing(radius)
+        return int(x), int(y)
+
+    @classmethod
+    def xy_id_to_qr(cls, x_id: int, y_id: int) -> Tuple[int, int]:
+        return x_id, y_id - x_id // 2
+
+    @classmethod
+    def xy_id_to_xy_coordinates(cls, x_id: int, y_id: int, radius: int) -> Tuple[int, int]:
+        return cls.qr_to_xy_coords(*cls.xy_id_to_qr(x_id=x_id, y_id=y_id), radius=radius)
+
+    @classmethod
+    def xy_coord_to_qr(cls, x: int, y: int, radius: int) -> Tuple[int, int]:
+        return cls.xy_id_to_qr(*cls.normalize_coordinates(x=x, y=y, radius=radius))
+
+    @classmethod
+    def cube_add(cls, h: Cube, vec: Cube) -> Cube:
+        return h + vec
+
+    @classmethod
+    def cube_scale(cls, h: Cube, factor: float) -> Cube:
+        return h.scale(factor)
+
+    @classmethod
+    def cube_neighbor(cls, cube: Cube, direction: int) -> Cube:
+        return cls.cube_add(cube, CUBE_VECTORS[direction])
+
+    @classmethod
+    def get_neighbors_qr(cls, q: int, r: int, range_radius: int = 1) -> List[Tuple[int, int]]:
+        results = []
+        for dq in range(-range_radius, range_radius + 1):
+            for dr in range(max(-range_radius, -dq - range_radius), min(range_radius, -dq + range_radius) + 1):
+                ds = -dq - dr
+                if abs(ds) <= range_radius:
+                    results.append((int(q + dq), int(r + dr)))
+        return results
+
+    @classmethod
+    def ring_at_radius(cls, q: int, r: int, range_radius: int) -> List[Tuple[int, int]]:
+        results = []
+        start_hex = cls.cube_add(Cube(q=q, r=r), cls.cube_scale(h=CUBE_VECTORS[4], factor=range_radius))
+
+        for i in range(6):
+            for j in range(range_radius):
+                results.append(start_hex.qr)
+                start_hex = cls.cube_neighbor(start_hex, i)
+        return results
+
+    @classmethod
+    def get_dots_by_qr(cls, q: int, r: int, radius: int):
+        return get_dots(*cls.qr_to_xy_coords(q=q, r=r, radius=radius), radius=radius)
+
+    @classmethod
+    def ring_at_radius_border(cls, q: int, r: int, range_radius: int, radius: int) -> List[Tuple[int, int]]:
+        results = []
+        start_hex = cls.cube_add(Cube(q, r), cls.cube_scale(CUBE_VECTORS[4], range_radius))
+        for i in range(6):
+            idxs = [3 - i, 2 - i, 1 - i]
+            for j in range(range_radius):
+                dots = cls.get_dots_by_qr(q=start_hex.q, r=start_hex.r, radius=radius)
+                for inx in idxs:
+                    if inx > 5:
+                        inx -= 6
+                    results.append(dots[inx])
+                start_hex = cls.cube_neighbor(start_hex, i)
+        return results
+
+    @classmethod
+    def get_cube_distance(cls, a: Cube, b: Cube) -> float:
+        vec = a - b
+        return (abs(vec.q) + abs(vec.r) + abs(vec.s)) / 2
+
+    @classmethod
+    def lerp(cls, a: Union[int, float], b: Union[int, float], t: Union[int, float]) -> int:
+        return int(a + (b - a) * t)
+
+    @classmethod
+    def cube_lerp(cls, a: Cube, b: Cube, t: Union[int, float]) -> Cube:
+        return Cube(cls.lerp(a.q, b.q, t), cls.lerp(a.r, b.r, t), cls.lerp(a.s, b.s, t))
+
+    @classmethod
+    def ray_from_a_to_b(cls, a: Cube, b: Cube) -> List[Tuple[int, int]]:
+        distance = cls.get_cube_distance(a, b)
+        result = []
+        if distance == 0:
+            return result
+
+        for i in range(int(distance) + 1):
+            result.append(cls.cube_round(cls.cube_lerp(a, b, 1.0 / distance * i)).qr)
+
+        return result
+
+    @classmethod
+    def cube_round(cls, frac: Cube) -> Cube:
+        q = round(frac.q)
+        r = round(frac.r)
+        s = round(frac.s)
+
+        q_diff = abs(q - frac.q)
+        r_diff = abs(r - frac.r)
+        s_diff = abs(s - frac.s)
+
+        if q_diff > r_diff and q_diff > s_diff:
+            q = -r - s
+        elif r_diff > s_diff:
+            r = -q - s
+        else:
+            s = -q - r
+
+        return Cube(q, r, s)
